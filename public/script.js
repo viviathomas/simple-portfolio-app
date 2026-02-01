2ff6db779729c1acc39eeaf2dcfdd93365b6a17b
vthomas <vthomas@manh.com>
2026-02-01 10:22:36 +0530

    fix vercel

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ async function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("/register", {
+  const res = await fetch("/api/auth/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email, password })
@@ -17,7 +17,7 @@ async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("/login", {
+  const res = await fetch("/api/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
@@ -27,12 +27,12 @@ async function login() {
 
   document.getElementById("message").innerText = data.message;
 
-  // ✅ Redirect when login is successful
+  
   if (data.message === "Login successful") {
     localStorage.setItem("name", data.name);
     localStorage.setItem("email", data.email);
 
-    // small delay so user sees message
+    
     setTimeout(() => {
       window.location.href = "/portfolio.html";
     }, 500);
